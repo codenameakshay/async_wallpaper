b3e967351dcf0801927b5e5da87f9fcfa4b0fdd4
codenameakshay <akshaymaurya3006@gmail.com>
2024-09-05 12:02:51 +0530

    chore: add testing changes (wip)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -52,29 +52,29 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomePage> createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with RestorationMixin {
+class HomePageState extends State<HomePage> with RestorationMixin {
   // Restorable state variables for various wallpaper operations
-  final RestorableString _platformVersion = RestorableString('Unknown');
-  final RestorableString _wallpaperFileNative = RestorableString('Unknown');
-  final RestorableString _wallpaperFileHome = RestorableString('Unknown');
-  final RestorableString _wallpaperFileLock = RestorableString('Unknown');
-  final RestorableString _wallpaperFileBoth = RestorableString('Unknown');
-  final RestorableString _wallpaperUrlNative = RestorableString('Unknown');
-  final RestorableString _wallpaperUrlHome = RestorableString('Unknown');
-  final RestorableString _wallpaperUrlLock = RestorableString('Unknown');
-  final RestorableString _wallpaperUrlBoth = RestorableString('Unknown');
-  final RestorableString _liveWallpaper = RestorableString('Unknown');
-  final RestorableString _wallpaperChooser = RestorableString('Unknown');
+  final RestorableString platformVersion = RestorableString('Unknown');
+  final RestorableString wallpaperFileNative = RestorableString('Unknown');
+  final RestorableString wallpaperFileHome = RestorableString('Unknown');
+  final RestorableString wallpaperFileLock = RestorableString('Unknown');
+  final RestorableString wallpaperFileBoth = RestorableString('Unknown');
+  final RestorableString wallpaperUrlNative = RestorableString('Unknown');
+  final RestorableString wallpaperUrlHome = RestorableString('Unknown');
+  final RestorableString wallpaperUrlLock = RestorableString('Unknown');
+  final RestorableString wallpaperUrlBoth = RestorableString('Unknown');
+  final RestorableString liveWallpaper = RestorableString('Unknown');
+  final RestorableString wallpaperChooser = RestorableString('Unknown');
 
   // URLs for static and live wallpapers
   String url = 'https://images.unsplash.com/photo-1635593701810-3156162e184f';
   String liveUrl = 'https://github.com/codenameakshay/sample-data/raw/main/video3.mp4';
 
   late bool goToHome;
-  String? _loadingOption;
+  String? loadingOption;
 
   @override
   void initState() {
@@ -86,13 +86,13 @@ class _HomePageState extends State<HomePage> with RestorationMixin {
   // Initialize platform state
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
+    String _platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion = await AsyncWallpaper.platformVersion ?? 'Unknown platform version';
+      _platformVersion = await AsyncWallpaper.instance.platformVersion ?? 'Unknown platform version';
     } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      _platformVersion = 'Failed to get platform version.';
     }
 
     // If the widget was removed from the tree while the asynchronous platform
@@ -101,14 +101,14 @@ class _HomePageState extends State<HomePage> with RestorationMixin {
     if (!mounted) return;
 
     setState(() {
-      _platformVersion.value = platformVersion;
+      platformVersion.value = _platformVersion;
     });
   }
 
   // Generic method to set wallpaper and update UI
   Future<void> setWallpaper(Function wallpaperSetter, RestorableString status, String optionLabel) async {
     setState(() {
-      _loadingOption = optionLabel;
+      loadingOption = optionLabel;
       status.value = 'Loading';
     });
 
@@ -122,7 +122,7 @@ class _HomePageState extends State<HomePage> with RestorationMixin {
     if (!mounted) return;
 
     setState(() {
-      _loadingOption = null;
+      loadingOption = null;
       status.value = result;
     });
   }
@@ -136,140 +136,140 @@ class _HomePageState extends State<HomePage> with RestorationMixin {
         'subtitle': 'Set wallpaper using a file on native screen',
         'onTap': () => setWallpaper(() async {
               var file = await DefaultCacheManager().getSingleFile(url);
-              return AsyncWallpaper.setWallpaperFromFileNative(
+              return AsyncWallpaper.instance.setWallpaperFromFileNative(
                 filePath: file.path,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperFileNative, 'File Native'),
-        'status': _wallpaperFileNative
+            }, wallpaperFileNative, 'File Native'),
+        'status': wallpaperFileNative
       },
       {
         'label': 'File Home',
         'subtitle': 'Set wallpaper using a file on home screen',
         'onTap': () => setWallpaper(() async {
               var file = await DefaultCacheManager().getSingleFile(url);
-              return AsyncWallpaper.setWallpaperFromFile(
+              return AsyncWallpaper.instance.setWallpaperFromFile(
                 filePath: file.path,
                 wallpaperLocation: AsyncWallpaper.HOME_SCREEN,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperFileHome, 'File Home'),
-        'status': _wallpaperFileHome
+            }, wallpaperFileHome, 'File Home'),
+        'status': wallpaperFileHome
       },
       {
         'label': 'File Lock',
         'subtitle': 'Set wallpaper using a file on lock screen',
         'onTap': () => setWallpaper(() async {
               var file = await DefaultCacheManager().getSingleFile(url);
-              return AsyncWallpaper.setWallpaperFromFile(
+              return AsyncWallpaper.instance.setWallpaperFromFile(
                 filePath: file.path,
                 wallpaperLocation: AsyncWallpaper.LOCK_SCREEN,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperFileLock, 'File Lock'),
-        'status': _wallpaperFileLock
+            }, wallpaperFileLock, 'File Lock'),
+        'status': wallpaperFileLock
       },
       {
         'label': 'File Both',
         'subtitle': 'Set wallpaper using a file on both screens',
         'onTap': () => setWallpaper(() async {
               var file = await DefaultCacheManager().getSingleFile(url);
-              return AsyncWallpaper.setWallpaperFromFile(
+              return AsyncWallpaper.instance.setWallpaperFromFile(
                 filePath: file.path,
                 wallpaperLocation: AsyncWallpaper.BOTH_SCREENS,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperFileBoth, 'File Both'),
-        'status': _wallpaperFileBoth
+            }, wallpaperFileBoth, 'File Both'),
+        'status': wallpaperFileBoth
       },
       {
         'label': 'URL Native',
         'subtitle': 'Set wallpaper using URL on native screen',
         'onTap': () => setWallpaper(() {
-              return AsyncWallpaper.setWallpaperNative(
+              return AsyncWallpaper.instance.setWallpaperNative(
                 url: url,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperUrlNative, 'URL Native'),
-        'status': _wallpaperUrlNative
+            }, wallpaperUrlNative, 'URL Native'),
+        'status': wallpaperUrlNative
       },
       {
         'label': 'URL Home',
         'subtitle': 'Set wallpaper using URL on home screen',
         'onTap': () => setWallpaper(() {
-              return AsyncWallpaper.setWallpaper(
+              return AsyncWallpaper.instance.setWallpaper(
                 url: url,
                 wallpaperLocation: AsyncWallpaper.HOME_SCREEN,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperUrlHome, 'URL Home'),
-        'status': _wallpaperUrlHome
+            }, wallpaperUrlHome, 'URL Home'),
+        'status': wallpaperUrlHome
       },
       {
         'label': 'URL Lock',
         'subtitle': 'Set wallpaper using URL on lock screen',
         'onTap': () => setWallpaper(() {
-              return AsyncWallpaper.setWallpaper(
+              return AsyncWallpaper.instance.setWallpaper(
                 url: url,
                 wallpaperLocation: AsyncWallpaper.LOCK_SCREEN,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperUrlLock, 'URL Lock'),
-        'status': _wallpaperUrlLock
+            }, wallpaperUrlLock, 'URL Lock'),
+        'status': wallpaperUrlLock
       },
       {
         'label': 'URL Both',
         'subtitle': 'Set wallpaper using URL on both screens',
         'onTap': () => setWallpaper(() {
-              return AsyncWallpaper.setWallpaper(
+              return AsyncWallpaper.instance.setWallpaper(
                 url: url,
                 wallpaperLocation: AsyncWallpaper.BOTH_SCREENS,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperUrlBoth, 'URL Both'),
-        'status': _wallpaperUrlBoth
+            }, wallpaperUrlBoth, 'URL Both'),
+        'status': wallpaperUrlBoth
       },
       {
         'label': 'Live Wallpaper',
         'subtitle': 'Set live wallpaper',
         'onTap': () => setWallpaper(() async {
               var file = await DefaultCacheManager().getSingleFile(liveUrl);
-              return AsyncWallpaper.setLiveWallpaper(
+              return AsyncWallpaper.instance.setLiveWallpaper(
                 filePath: file.path,
                 goToHome: goToHome,
                 toastDetails: ToastDetails.success(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _liveWallpaper, 'Live Wallpaper'),
-        'status': _liveWallpaper
+            }, liveWallpaper, 'Live Wallpaper'),
+        'status': liveWallpaper
       },
       {
         'label': 'Wallpaper Chooser',
         'subtitle': 'Open wallpaper chooser',
         'onTap': () => setWallpaper(() {
-              return AsyncWallpaper.openWallpaperChooser(
+              return AsyncWallpaper.instance.openWallpaperChooser(
                 goToHome: goToHome,
                 toastDetails: ToastDetails.wallpaperChooser(),
                 errorToastDetails: ToastDetails.error(),
               );
-            }, _wallpaperChooser, 'Wallpaper Chooser'),
-        'status': _wallpaperChooser
+            }, wallpaperChooser, 'Wallpaper Chooser'),
+        'status': wallpaperChooser
       },
     ];
 
@@ -300,14 +300,14 @@ class _HomePageState extends State<HomePage> with RestorationMixin {
                 childAspectRatio: 1.0, // Make buttons square
                 children: wallpaperOptions.map((option) {
                   return ElevatedButton(
-                    onPressed: _loadingOption == null ? option['onTap'] : null,
+                    onPressed: loadingOption == null ? option['onTap'] : null,
                     style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12.0),
                       ),
                       padding: const EdgeInsets.symmetric(horizontal: 4),
                     ),
-                    child: _loadingOption == option['label']
+                    child: loadingOption == option['label']
                         ? const Center(child: CircularProgressIndicator())
                         : Column(
                             mainAxisAlignment: MainAxisAlignment.center,
@@ -342,20 +342,20 @@ class _HomePageState extends State<HomePage> with RestorationMixin {
 
   // Restoration
   @override
-  String? get restorationId => '_HomePageState';
+  String? get restorationId => 'HomePageState';
 
   @override
   void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
-    registerForRestoration(_platformVersion, 'platform_version');
-    registerForRestoration(_wallpaperFileNative, 'wallpaper_file_native');
-    registerForRestoration(_wallpaperFileHome, 'wallpaper_file_home');
-    registerForRestoration(_wallpaperFileLock, 'wallpaper_file_lock');
-    registerForRestoration(_wallpaperFileBoth, 'wallpaper_file_both');
-    registerForRestoration(_wallpaperUrlNative, 'wallpaper_url_native');
-    registerForRestoration(_wallpaperUrlHome, 'wallpaper_url_home');
-    registerForRestoration(_wallpaperUrlLock, 'wallpaper_url_lock');
-    registerForRestoration(_wallpaperUrlBoth, 'wallpaper_url_both');
-    registerForRestoration(_liveWallpaper, 'live_wallpaper');
-    registerForRestoration(_wallpaperChooser, 'wallpaper_chooser');
+    registerForRestoration(platformVersion, 'platform_version');
+    registerForRestoration(wallpaperFileNative, 'wallpaper_file_native');
+    registerForRestoration(wallpaperFileHome, 'wallpaper_file_home');
+    registerForRestoration(wallpaperFileLock, 'wallpaper_file_lock');
+    registerForRestoration(wallpaperFileBoth, 'wallpaper_file_both');
+    registerForRestoration(wallpaperUrlNative, 'wallpaper_url_native');
+    registerForRestoration(wallpaperUrlHome, 'wallpaper_url_home');
+    registerForRestoration(wallpaperUrlLock, 'wallpaper_url_lock');
+    registerForRestoration(wallpaperUrlBoth, 'wallpaper_url_both');
+    registerForRestoration(liveWallpaper, 'live_wallpaper');
+    registerForRestoration(wallpaperChooser, 'wallpaper_chooser');
   }
 }
--- test/async_wallpaper_test.dart
+++ test/async_wallpaper_test.dart
@@ -1,23 +1,140 @@
+import 'package:async_wallpaper/async_wallpaper.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-import 'package:async_wallpaper/async_wallpaper.dart';
+class MockMethodChannel extends Mock implements MethodChannel {}
 
 void main() {
-  const channel = MethodChannel('async_wallpaper');
-  TestWidgetsFlutterBinding.ensureInitialized();
+  late AsyncWallpaper asyncWallpaper;
+  late MockMethodChannel mockChannel;
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+    mockChannel = MockMethodChannel();
+    asyncWallpaper = AsyncWallpaper.instance;
+    asyncWallpaper.channel = mockChannel;
+  });
+
+  group('Async Wallpaper tests', () {
+    test('platformVersion', () async {
+      when(() async => await mockChannel.invokeMethod<String>('getPlatformVersion'))
+          .thenAnswer((_) async => 'Test Version');
+
+      expect(await asyncWallpaper.platformVersion, 'Test Version');
     });
-  });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+    test('setWallpaper', () async {
+      when(() => mockChannel.invokeMethod<bool>('set_both_wallpaper', any())).thenAnswer((_) async => true);
 
-  test('getPlatformVersion', () async {
-    expect(await AsyncWallpaper.platformVersion, '42');
+      expect(
+        await asyncWallpaper.setWallpaper(url: 'https://example.com/image.jpg'),
+        true,
+      );
+    });
+
+    test('setWallpaperNative', () async {
+      when(() => mockChannel.invokeMethod<bool>('set_wallpaper', any())).thenAnswer((_) async => true);
+
+      expect(
+        await asyncWallpaper.setWallpaperNative(url: 'https://example.com/image.jpg'),
+        true,
+      );
+    });
+
+    test('setWallpaperFromFileNative', () async {
+      when(() => mockChannel.invokeMethod<bool>('set_wallpaper_file', any())).thenAnswer((_) async => true);
+
+      expect(
+        await asyncWallpaper.setWallpaperFromFileNative(filePath: '/path/to/image.jpg'),
+        true,
+      );
+    });
+
+    test('setWallpaperFromFile', () async {
+      when(() => mockChannel.invokeMethod<bool>('set_both_wallpaper_file', any())).thenAnswer((_) async => true);
+
+      expect(
+        await asyncWallpaper.setWallpaperFromFile(filePath: '/path/to/image.jpg'),
+        true,
+      );
+    });
+
+    test('setLiveWallpaper', () async {
+      when(() => mockChannel.invokeMethod<bool>('set_video_wallpaper', any())).thenAnswer((_) async => true);
+
+      expect(
+        await asyncWallpaper.setLiveWallpaper(filePath: '/path/to/video.mp4'),
+        true,
+      );
+    });
+
+    test('openWallpaperChooser', () async {
+      when(() => mockChannel.invokeMethod<bool>('open_wallpaper_chooser', any())).thenAnswer((_) async => true);
+
+      expect(
+        await asyncWallpaper.openWallpaperChooser(),
+        true,
+      );
+    });
+
+    test('setWallpaper with different locations', () async {
+      when(() => mockChannel.invokeMethod<bool>('set_home_wallpaper', any())).thenAnswer((_) async => true);
+      when(() => mockChannel.invokeMethod<bool>('set_lock_wallpaper', any())).thenAnswer((_) async => true);
+      when(() => mockChannel.invokeMethod<bool>('set_both_wallpaper', any())).thenAnswer((_) async => true);
+
+      expect(
+        await asyncWallpaper.setWallpaper(
+          url: 'https://example.com/image.jpg',
+          wallpaperLocation: AsyncWallpaper.HOME_SCREEN,
+        ),
+        true,
+      );
+
+      expect(
+        await asyncWallpaper.setWallpaper(
+          url: 'https://example.com/image.jpg',
+          wallpaperLocation: AsyncWallpaper.LOCK_SCREEN,
+        ),
+        true,
+      );
+
+      expect(
+        await asyncWallpaper.setWallpaper(
+          url: 'https://example.com/image.jpg',
+          wallpaperLocation: AsyncWallpaper.BOTH_SCREENS,
+        ),
+        true,
+      );
+    });
+
+    test('setWallpaperFromFile with different locations', () async {
+      when(() => mockChannel.invokeMethod<bool>('set_home_wallpaper_file', any())).thenAnswer((_) async => true);
+      when(() => mockChannel.invokeMethod<bool>('set_lock_wallpaper_file', any())).thenAnswer((_) async => true);
+      when(() => mockChannel.invokeMethod<bool>('set_both_wallpaper_file', any())).thenAnswer((_) async => true);
+
+      expect(
+        await asyncWallpaper.setWallpaperFromFile(
+          filePath: '/path/to/image.jpg',
+          wallpaperLocation: AsyncWallpaper.HOME_SCREEN,
+        ),
+        true,
+      );
+
+      expect(
+        await asyncWallpaper.setWallpaperFromFile(
+          filePath: '/path/to/image.jpg',
+          wallpaperLocation: AsyncWallpaper.LOCK_SCREEN,
+        ),
+        true,
+      );
+
+      expect(
+        await asyncWallpaper.setWallpaperFromFile(
+          filePath: '/path/to/image.jpg',
+          wallpaperLocation: AsyncWallpaper.BOTH_SCREENS,
+        ),
+        true,
+      );
+    });
   });
 }
